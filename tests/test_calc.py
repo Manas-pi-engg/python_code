1ad565e1f3f3ac59892f8d98e7c57af9a66fa764
Manas-pi-engg <122532804+Manas-pi-engg@users.noreply.github.com>
2023-02-27 09:48:02 +0530

    Create test_calc.py

---
+++ tests/test_calc.py
@@ -0,0 +1,16 @@
+import unittest
+import calc
+
+class test_calc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calc.add(2, 3), 5)
+
+    def test_sub(self):
+        self.assertEqual(calc.sub(2, 3), -1)
+        
+    def test_mul(self):
+        self.assertEqual(calc.mul(2, 3), 6)
+        
+if __name__ == '__main__':
+    unittest.main()

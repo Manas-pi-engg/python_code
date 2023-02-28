87dd7055559fe77385a55f77515b3010c192e30c
manas945 <manasagwl@gmail.com>
2023-02-28 13:19:59 +0530

    added mul

--- src/test_calc.py
+++ src/test_calc.py
@@ -8,6 +8,9 @@ class test_calc(unittest.TestCase):
 
     def test_sub(self):
         self.assertEqual(calc.sub(2, 3), -1)
+
+    def test_sub(self):
+        self.assertEqual(calc.mul(2, 3), 6)
         
 
 if __name__ == '__main__':

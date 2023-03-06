6cd2f1f106aafddd58875e68ee57af9b823ff08f
Manas-pi-engg <122532804+Manas-pi-engg@users.noreply.github.com>
2023-03-06 12:04:18 +0530

    Update test_calc.py

--- src/test_calc.py
+++ src/test_calc.py
@@ -10,15 +10,14 @@ class test_calc(unittest.TestCase):
     def test_sub(self):
         self.assertEqual(calc.sub(2, 3), -1)
 
-    def test_sub(self):
+    def test_sub1(self):
         self.assertEqual(calc.mul(2, 3), 6)
-    def test_add(self):
-            self.assertEqual(cal.OR(2 , 2), 2)
-    def test_add(self):
+
+    def test_add1(self):
             self.assertEqual(cal.add(2 , 3,4), 9)
-    def test_add(self):
+    def test_add2(self):
             self.assertEqual(cal.sub(2 , 3,4), 1)
-    def test_add(self):
+    def test_add3(self):
             self.assertEqual(cal.mul(2 , 3,4), 24)
 
 if __name__ == '__main__':
